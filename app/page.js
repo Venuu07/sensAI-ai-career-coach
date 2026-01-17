832df0396919e925adf5f5a1541faad3c7308044
Venuu07 <venuchinthakunta7@gmail.com>
2026-01-17 17:33:01 +0530

    inital next js config

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-4xl justify-center">
+        hello venu
+      </div>
+    </> 
+  );
+}
