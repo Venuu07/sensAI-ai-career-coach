76a6f3f8ce7ee61b77640a93d31c3bf8ad75bb0c
Venuu07 <venuchinthakunta7@gmail.com>
2026-01-20 00:47:45 +0530

    design landing page

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import { helloWorld } from "@/lib/inngest/functions";
+// We will import our functions here later, for now it's an empty array
+// import { helloWorld } from "@/lib/inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    helloWorld,
+  ],
+});
---
+++ data/testimonial.js
@@ -0,0 +1,34 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Suresh Lewis",
+    image: "https://randomuser.me/api/portraits/men/34.jpg",
+    role: "Software Engineer",
+    company: "Tech Giant Co.",
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Shiva Nichols",
+    image: "https://randomuser.me/api/portraits/men/10.jpg",
+    role: "Product Manager",
+    company: "StartUp Inc.",
+  },
+  {
+    quote:
+      "Writing cover letters used to be a nightmare. SensAI turned hours of work into minutes, and the quality was better than what I wrote myself!",
+    author: "Bunny Keller ",
+    image: "https://randomuser.me/api/portraits/men/41.jpg", // Changed ID for variety
+    role: "DevOps Engineer",
+    company: "Cloud Systems",
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Shailaja Bubble",
+    image: "https://randomuser.me/api/portraits/women/84.jpg",
+    role: "Marketing Director",
+    company: "Global Corp",
+  },
+];
